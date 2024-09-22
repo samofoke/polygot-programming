edc6ea7c0c66c179b67c3564e6168ed76d463a75
samofoke <sabataernestmofokeng@gmail.com>
2024-09-22 17:52:44 +0200

    building small

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # polygot-programming
 ## This Project is building CLI tool in 3 langauges Ts, rs and go.
+-  We sart buidling a types for our arguments for our cli.
---
+++ cli-typescript/src/components/types/ops/ops.ts
@@ -0,0 +1,27 @@
+import cli from "command-line-args";
+
+export type Opts = {
+  args?: string[];
+  pwd?: string;
+  config?: string;
+};
+
+export default function getOpts(): Opts {
+  return cli([
+    {
+      name: "args",
+      defaultValue: true,
+      type: String,
+    },
+    {
+      name: "config",
+      alias: "c",
+      type: String,
+    },
+    {
+      name: "pwd",
+      alias: "p",
+      type: String,
+    },
+  ]) as Opts;
+}
