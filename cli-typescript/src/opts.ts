5e84fc85b05f18fb8ef2124c36a630b66ef2bf41
samofoke <sabataernestmofokeng@gmail.com>
2024-09-23 20:32:34 +0200

    config the files

---
+++ cli-typescript/src/opts.ts
@@ -0,0 +1,27 @@
+import cli from "command-line-args";
+
+export type Opts = {
+  args?: string[];
+  pwd?: string;
+  config?: string;
+};
+
+export default function getOpts(): Opts {
+  return cli([
+    {
+      name: "args",
+      defaultOption: true,
+      type: String,
+    },
+    {
+      name: "pwd",
+      alias: "p",
+      type: String,
+    },
+    {
+      name: "config",
+      alias: "c",
+      type: String,
+    },
+  ]) as Opts;
+}
