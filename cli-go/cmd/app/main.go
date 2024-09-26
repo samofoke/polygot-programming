abc2931db8de51275d2f37e9ba14b0fc0abfb447
samofoke <sabataernestmofokeng@gmail.com>
2024-09-26 17:28:11 +0200

    added the oporation function

--- cli-go/cmd/app/main.go
+++ cli-go/cmd/app/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/samofoke/polygot/pkg/progfile"
+)
+
+func main() {
+	opts, err := progfile.GetOps()
+	if err != nil {
+		log.Fatalf("unable to get options %v", err)
+	}
+
+	fmt.Printf("Operations: %+v", opts)
+}
--- cli-go/go.mod
+++ cli-go/go.mod
@@ -1,3 +1,5 @@
 module github.com/samofoke/polygot
 
 go 1.23.0
+
+require github.com/hellflame/argparse v1.12.0 // indirect
